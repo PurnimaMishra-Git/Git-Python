7bf3ce1a4f7724409160591e0e72ab4d5961e8f8
PurnimaMishra-Git <84815829+PurnimaMishra-Git@users.noreply.github.com>
2021-05-28 08:16:57 +0530

    Update FaultyCalculator.py

--- FaultyCalculator.py
+++ FaultyCalculator.py
@@ -9,8 +9,6 @@ number2=int(input("Enter number2: "))
 if list[0]== a:
     if number1== 56 and number2== 6:
         print("56+6=77")
-        print(type(a))
-        print(type(list[0]))
     else:print(f"{number1}+{number2}=", number1+number2)
 elif list[1] == a:
     if number1 == 45 and number2 == 3:
